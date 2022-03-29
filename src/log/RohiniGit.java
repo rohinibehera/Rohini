19de7bb48bb33412e1d4bc5f92caa548866e22ff
rohinibehera <102520425+rohinibehera@users.noreply.github.com>
2022-03-29 15:47:44 +0530

    demo
    print

--- src/log/RohiniGit.java
+++ src/log/RohiniGit.java
@@ -2,13 +2,10 @@ package log;
 
 public class RohiniGit {
 
-	private void Syso() {
-		// TODO Auto-generated method stub
-
-	}
 	
-
 	public static void main(String[] args) {
+		
+		System.out.println("hello");
 		
 
 	}
